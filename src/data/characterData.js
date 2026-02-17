142c5d37c0266834a26e2fead03708268f0dda42
Taylor Bonnesen <tbonnesen@Mac.lan>
2026-02-16 19:04:38 -0600

    Added comprehensive character gallery images and updated metadata

--- src/data/characterData.js
+++ src/data/characterData.js
@@ -15,6 +15,248 @@ export const characterData = {
         image: "/images/characters/david.png",
         color: "#ffd700"
     },
+    "Animals": {
+        image: "/images/characters/animals.png",
+        color: "#aed581"
+    },
+    "Sheep": {
+        image: "/images/characters/sheep.png",
+        color: "#fff59d"
+    },
+    "Moses": {
+        image: "/images/characters/moses.png",
+        color: "#fa8479"
+    },
+    "Jonah": {
+        image: "/images/characters/jonah.png",
+        color: "#4facfe"
+    },
+    "Esther": {
+        image: "/images/characters/esther.png",
+        color: "#f48fb1"
+    },
+    "Daniel": {
+        image: "/images/characters/daniel.png",
+        color: "#fea500"
+    },
+    "Samson": {
+        image: "/images/characters/samson.png",
+        color: "#ffd54f"
+    },
+    "Abraham": {
+        image: "/images/characters/abraham.png",
+        color: "#b39ddb"
+    },
+    "Elijah": {
+        image: "/images/characters/elijah.png",
+        color: "#90a4ae"
+    },
+    "Adam": {
+        image: "/images/characters/adam.png",
+        color: "#66bb6a"
+    },
+    "Eve": {
+        image: "/images/characters/eve.png",
+        color: "#a5d6a7"
+    },
+    "Joseph": {
+        image: "/images/characters/joseph.png",
+        color: "#84fab0"
+    },
+    "Mary": {
+        image: "/images/characters/mary.png",
+        color: "#8fd3f4"
+    },
+    "Ruth": {
+        image: "/images/characters/ruth.png",
+        color: "#ffcca0"
+    },
+    "Samuel": {
+        image: "/images/characters/samuel.png",
+        color: "#a5d6a7"
+    },
+    "Peter": {
+        image: "/images/characters/peter.png",
+        color: "#4dd0e1"
+    },
+    "Gideon": {
+        image: "/images/characters/gideon.png",
+        color: "#81d4fa"
+    },
+    // John the Baptist image failed, skipping for now
+    "Elisha": {
+        image: "/images/characters/elisha.png",
+        color: "#ffab91"
+    },
+    "John the Baptist": {
+        image: "/images/characters/john_the_baptist.png",
+        color: "#4fc3f7"
+    },
+    "Jacob": {
+        image: "/images/characters/jacob.png",
+        color: "#80cbc4"
+    },
+    "Zacchaeus": {
+        image: "/images/characters/zacchaeus.png",
+        color: "#dce775"
+    },
+    "Joshua": {
+        image: "/images/characters/joshua.png",
+        color: "#ff7043"
+    },
+    "Shadrach": {
+        image: "/images/characters/shadrach_meshach_abednego.png",
+        color: "#ef5350"
+    },
+    "Meshach": {
+        image: "/images/characters/shadrach_meshach_abednego.png",
+        color: "#ef5350"
+    },
+    "Abednego": {
+        image: "/images/characters/shadrach_meshach_abednego.png",
+        color: "#ef5350"
+    },
+    "Balaam": {
+        image: "/images/characters/balaam.png",
+        color: "#9575cd"
+    },
+    "Donkey": {
+        image: "/images/characters/balaam.png",
+        color: "#d1c4e9"
+    },
+    "Good Samaritan": {
+        image: "/images/characters/good_samaritan.png",
+        color: "#f6d365"
+    },
+    "Goliath": {
+        image: "/images/characters/goliath.png",
+        color: "#ff8a65"
+    },
+    "Whale": {
+        image: "/images/characters/whale.png",
+        color: "#90caf9"
+    },
+    "Lions": {
+        image: "/images/characters/lions.png",
+        color: "#ffcc80"
+    },
+    // Using singular 'angel' image for plural 'Angels' too for now
+    "Angels": {
+        image: "/images/characters/angel.png",
+        color: "#fff59d"
+    },
+    "Angel": {
+        image: "/images/characters/angel.png",
+        color: "#fff59d"
+    },
+    "Shepherd": {
+        image: "/images/characters/shepherd.png",
+        color: "#aed581"
+    },
+    "Naomi": {
+        image: "/images/characters/naomi.png",
+        color: "#ba68c8"
+    },
+    "King Xerxes": {
+        image: "/images/characters/king_xerxes.png",
+        color: "#1e88e5"
+    },
+    "Miriam": {
+        image: "/images/characters/miriam.png",
+        color: "#f06292"
+    },
+    "Wise Men": {
+        image: "/images/characters/wise_men.png",
+        color: "#ffd700"
+    },
+    "Princess": {
+        image: "/images/characters/princess.png",
+        color: "#ff80ab"
+    },
+    "Snake": {
+        image: "/images/characters/snake.png",
+        color: "#81c784"
+    },
+    "Holy Spirit": {
+        image: "/images/characters/holy_spirit.png",
+        color: "#b3e5fc"
+    },
+    "Eli": {
+        image: "/images/characters/eli.png",
+        color: "#b0bec5"
+    },
+    "Mordecai": {
+        image: "/images/characters/mordecai.png",
+        color: "#5c6bc0"
+    },
+    "King Nebuchadnezzar": {
+        image: "/images/characters/nebuchadnezzar.png",
+        color: "#ef6c00"
+    },
+    "King Darius": {
+        image: "/images/characters/king_darius.png",
+        color: "#5e35b1"
+    },
+    "Birds": {
+        image: "/images/characters/birds.png",
+        color: "#424242"
+    },
+    "Farmer": {
+        image: "/images/characters/farmer.png",
+        color: "#8d6e63"
+    },
+    "Woman": {
+        image: "/images/characters/woman.png",
+        color: "#ba68c8"
+    },
+    "Jochebed": {
+        image: "/images/characters/jochebed.png",
+        color: "#f06292"
+    },
+    "Boy": {
+        image: "/images/characters/boy.png",
+        color: "#ffcc80"
+    },
+    "Disciples": {
+        image: "/images/characters/disciples.png",
+        color: "#90caf9"
+    },
+    "Paralyzed Man": {
+        image: "/images/characters/paralyzed_man.png",
+        color: "#ffe082"
+    },
+    "Children": {
+        image: "/images/characters/children.png",
+        color: "#aed581"
+    },
+    "Friends": {
+        image: "/images/characters/disciples.png",
+        color: "#ffab91"
+    },
+    "Brothers": {
+        image: "/images/characters/disciples.png",
+        color: "#ef9a9a"
+    },
+    "Servant": {
+        image: "/images/characters/boy.png",
+        color: "#b0bec5"
+    },
+    "Priests": {
+        image: "/images/characters/disciples.png",
+        color: "#e6ee9c"
+    },
+    "Innkeeper": {
+        image: "/images/characters/farmer.png",
+        color: "#d7ccc8"
+    },
+    "Israelites": {
+        image: "/images/characters/disciples.png",
+        color: "#b3e5fc"
+    },
+    "People": {
+        image: "/images/characters/disciples.png",
+        color: "#ffcc80"
+    },
     // Add more characters here as we generate images
     "God": {
         image: null, // Placeholder to show default avatar
